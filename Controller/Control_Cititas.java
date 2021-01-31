c9850df45b4248e548284bd28e7d09a245a3270a
cegch170775 <carlosgualpach@hotmail.com>
2021-01-31 18:33:20 -0500

    Citas V1.3
    Incorporando módulos contoller,view y resource

---
+++ Controller/Control_Cititas.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author CARLOS GUALPA
+ */
+public class Control_Cititas {
+    
+}
